793d9baa90e8d41c4518749bf917fb10dad0f342
NIHILncunia <nihil_ncunia@naver.com>
2025-03-24 01:59:24 +0900

    2025 0324 타이틀, 설명등 기본 정보 추가.

--- app/_config/site.config.ts
+++ app/_config/site.config.ts
@@ -2,9 +2,9 @@ import { nihilLogo, nihilLogoWhite } from '@/_images';
 import { SiteConfig } from '@/_types/common.types';
 
 export const siteConfig: SiteConfig = {
-  title: '사이트 이름',
-  description: '사이트 설명',
-  keywords: '',
+  title: '랜덤 키워드',
+  description: '창작자들의 영감을 위한 랜덤 키워드 생성기',
+  keywords: '키워드,창작,그림쟁이,글쟁이,랜덤키워드,keyword,creative,illustrator,writer,randomkeyword',
   author: {
     name: 'NIHILncunia',
     url: 'https://github.com/NIHILncunia',
