232efb4be47c7ed45d7202739e6e44608bbc8dde
NIHILncunia <nihil_ncunia@naver.com>
2025-04-15 01:06:12 +0900

    2025 0415 디버그 로그 제거

--- app/_config/site.config.ts
+++ app/_config/site.config.ts
@@ -12,7 +12,6 @@ export const siteConfig: SiteConfig = {
   type: 'website',
   url: process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    // : 'http://localhost:3000',
     : 'https://randomkeywords.nihilapps.dev',
   cover: {
     link: '/opengraph-image.png',
